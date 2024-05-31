b7953491bb7862bc4053306fdf6b312727a0b4ee
Isaac Ndirangu Muturi <ndirangumuturi749@gmail.com>
2024-05-31 12:55:28 +0300

    update task 0

---
+++ 0x01-ES6_promise/0-promise.js
@@ -0,0 +1,7 @@
+/* eslint-disable no-unused-vars */
+export default function getResponseFromAPI() {
+  return new Promise((resolve, reject) => {
+    // For now, the promise can be resolved immediately
+    resolve('API response');
+  });
+}
