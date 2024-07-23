d1599d16e2030f80e4db36bb2a8b4b5e12064131
Isaac Ndirangu Muturi <ndirangumuturi749@gmail.com>
2024-07-23 19:42:46 +0300

    Update 4-http.js

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,16 +1,17 @@
 const http = require('http');
 
 const PORT = 1245;
+const hostname = '127.0.0.1';
 
 const app = http.createServer((req, res) => {
   // Set the response header
   res.writeHead(200, { 'Content-Type': 'text/plain' });
   // Send the response body
-  res.end('Hello Holberton School!\n');
+  res.end('Hello Holberton School!');
 });
 
 // Start the server and listen on the specified port
-app.listen(PORT, () => {
+app.listen(PORT, hostname, () => {
   console.log(`Server is listening on port ${PORT}`);
 });
 
