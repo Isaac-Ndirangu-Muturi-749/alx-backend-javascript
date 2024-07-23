673bad83c09555ed6bb575fa69dc5f072190e95f
Isaac Ndirangu Muturi <ndirangumuturi749@gmail.com>
2024-07-23 16:53:17 +0300

    task 0

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,11 @@
+// 0-console.js
+
+/**
+ * Prints a message to STDOUT
+ * @param {string} message - The message to print
+ */
+function displayMessage(message) {
+    console.log(message);
+  }
+
+  module.exports = displayMessage;
