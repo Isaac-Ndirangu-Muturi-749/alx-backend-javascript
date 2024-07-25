5348a079e5f356e69c8b3ad878d6e30721776e66
Isaac Ndirangu Muturi <ndirangumuturi749@gmail.com>
2024-07-25 17:20:23 +0300

    task 3

---
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -0,0 +1,14 @@
+// 3-payment.test.js
+const sinon = require('sinon');
+const { expect } = require('chai');
+const Utils = require('./utils');
+const sendPaymentRequestToApi = require('./3-payment');
+
+describe('sendPaymentRequestToApi', () => {
+  it('should use Utils.calculateNumber with type SUM', () => {
+    const spy = sinon.spy(Utils, 'calculateNumber');
+    sendPaymentRequestToApi(100, 20);
+    expect(spy.calledWith('SUM', 100, 20)).to.be.true;
+    spy.restore();
+  });
+});
